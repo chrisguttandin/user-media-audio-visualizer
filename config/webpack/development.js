e34cdc2471202d8126471d9a3eeb4cca84ce231e
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-03-27 00:31:50 +0200

    build(config): update grunt config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,6 +1,11 @@
 const { resolve } = require('path');
 
 module.exports = {
+    devServer: {
+        compress: true,
+        port: 3311,
+        static: './src'
+    },
     devtool: 'source-map',
     entry: {
         app: './src/scripts/app.js'
